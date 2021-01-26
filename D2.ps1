4963bfe2ac9758e2954041452a14b60b9bdd72f4
n0isegat3 <jan@cyber-rangers.com>
2021-01-26 11:57:24 +0100

    u

--- D2.ps1
+++ D2.ps1
@@ -94,3 +94,180 @@ Get-ADComputer -Filter * | ForEach-Object {
 Get-ADComputer -Filter * | ForEach-Object {
     Invoke-Command -ComputerName $_.DnsHostname -ScriptBlock {$env:computername}
 }
+
+get-help get-service -full
+
+Get-Service -Name Spooler
+
+'spooler' | Get-Service
+
+
+
+'this is content of a file' | Out-File -FilePath C:\Temp\fakelog.txt
+Get-Content C:\Temp\fakelog.txt
+'this is additional content of a file' | Out-File -FilePath C:\Temp\fakelog.txt -Append
+Get-Content C:\Temp\fakelog.txt
+
+
+
+
+
+
+
+
+
+
+
+Get-Volume | Select-Object -Property DriveType, HealthStatus, Size, SizeRemaining
+Get-Volume | Select-Object -Property *
+
+Get-ADUser -Filter * -Properties PhysicalDeliveryOfficeName | Select-Object -Property *
+
+Get-ADUser -Filter * -Properties * | Select-Object -Property *
+
+
+
+
+Get-Process | Select-Object -Property ProcessName,Id,VM -First 5
+
+Get-Process | Sort-Object -Property Id | Select-Object -First 5
+
+Get-Process | Select-Object -Property ProcessName -Unique
+
+Get-Process | Group-Object -Property ProcessName
+
+
+
+
+
+Get-ChildItem C:\Temp | Get-Member
+
+New-Item -Path C:\Temp\ToEncrypt -ItemType Directory
+
+Get-ChildItem C:\Temp\ToEncrypt
+
+1..100 | ForEach-Object {
+    New-Item -ItemType File -Path C:\Temp\ToEncrypt -Name "FileToEncrypt$_"
+    'data to file' | Out-File -FilePath "C:\Temp\ToEncrypt\FileToEncrypt$_"
+}
+
+Get-ChildItem C:\Temp\ToEncrypt | ForEach-Object {
+    $_.FullName
+    $_.Encrypt()
+}
+
+101..200 | ForEach-Object {
+    New-Item -ItemType File -Path C:\Temp\ToEncrypt -Name "FileToEncrypt$_"
+    'data to file' | Out-File -FilePath "C:\Temp\ToEncrypt\FileToEncrypt$_"
+}
+
+Get-ChildItem C:\Temp\ToEncrypt | ForEach-Object {
+    $_.FullName
+}
+Get-ChildItem C:\Temp\ToEncrypt | ForEach-Object {
+    $_.Encrypt()
+}
+Get-ChildItem C:\Temp\ToEncrypt | ForEach-Object -MemberName Encrypt
+
+
+
+
+Get-Process | Select-Object -First 2 -Property ProcessName,Id,PM,@{name='PM(MB)';expression={$_.PM/1024/1024}}
+
+Get-Process | Select-Object -First 2 -Property ProcessName,Id,PM,@{name='PM(MB)';expression={$_.PM/1MB}}
+
+Get-Process | Select-Object -First 2 -Property ProcessName,Id,PM,@{n='PM(MB)';e={$_.PM/1MB}}
+
+Get-Process | Select-Object -First 2 -Property ProcessName,Id,PM,@{n='PM(MB)';e={$_.PM/1MB}},@{n='nonsense';e={'whoooo'}} | Format-Table
+
+
+4+5
+3-1
+4/2
+5%2 #modulus
+4*4
+
+1kB
+1MB
+1GB
+1TB
+1PB
+
+
+Get-Process | Select-Object -f 5 | Format-List
+Get-WmiObject win32_logicaldisk | Format-Table -AutoSize
+
+
+
+
+Get-Service | Select-Object -Property StartType,Name,DisplayName,DependentServices,MachineName | Format-Table -AutoSize -Wrap
+
+
+
+
+Get-Process -Name svchost
+Get-Process -Id 4
+
+Get-Process -Name svchost
+Get-Process | Where-Object -Property Name -EQ -Value svchost
+
+Get-Process | Where-Object -FilterScript {$_.Name -eq 'svchost'}
+Get-Process | Where-Object {$_.Name -eq 'svchost'}
+Get-Process | Where {$_.Name -eq 'svchost'}
+Get-Process | ? {$_.Name -eq 'svchost'}
+
+
+
+5 -gt 5
+5 -ge 5
+
+'text1' -eq 'TEXT1'
+
+'text1' -ceq 'TEXT1'
+
+'text1' -like '*ext*'
+
+'text1' -clike '*eXT*'
+'text1' -like '*eXT*'
+
+
+'text1' -eq 'TEXT1'
+'text1' -ceq 'TEXT1'
+'text1' -ieq 'TEXT1'
+
+'mylib.dll' -match '^m(.{3,5})\.dll$'
+'alib.dll' -match '^m(.{3,5})\.dll$'
+
+
+
+Get-Process | Where-Object {$_.PM -gt 100MB}
+
+
+
+Get-Process | Out-GridView
+
+
+Get-Process | Out-GridView -PassThru -Title 'Select Process'
+
+
+'create','delete','show' | Out-GridView -PassThru -Title 'select operation'
+
+
+Get-Process | Where-Object {$_.PM -gt 100MB} | Select-Object ProcessName,Id,@{n='PM_MB';e={$_.PM/1MB}} | Out-Printer
+
+Get-Process | Where-Object {$_.PM -gt 100MB} | Select-Object ProcessName,Id,@{n='PM_MB';e={$_.PM/1MB}} | Export-Csv -Path C:\Temp\myprocs1.csv -Delimiter "`t"
+
+Import-Csv -Delimiter "`t" -Path C:\Temp\myprocs1.csv | gm
+
+Get-Process | Where-Object {$_.PM -gt 100MB} | Select-Object ProcessName,Id,@{n='PM_MB';e={$_.PM/1MB}} | Export-Csv -Path C:\Temp\myprocs2.csv -Delimiter "`t" -NoTypeInformation
+
+Import-Csv -Delimiter "`t" -Path C:\Temp\myprocs2.csv | gm
+
+
+Get-Process | Where-Object {$_.PM -gt 100MB} | Select-Object ProcessName,Id,@{n='PM_MB';e={$_.PM/1MB}} | ConvertTo-Html | Out-File -FilePath C:\Temp\myprocs3.html
+
+
+Get-Process | Where-Object {$_.PM -gt 100MB} | Select-Object ProcessName,Id,@{n='PM_MB';e={$_.PM/1MB}} | ConvertTo-Html | Out-Printer
+
+
+Get-Process | Where-Object {$_.PM -gt 100MB} | Select-Object ProcessName,Id,@{n='PM_MB';e={$_.PM/1MB}} | ConvertTo-Html
---
+++ Scripts/mod002-002.ps1
@@ -0,0 +1,40 @@
+﻿#object processing
+Get-Process | Sort-Object -Property Name -Descending
+
+Get-Process | Select-Object -Property Name,ID
+Get-Process | Select-Object -Property Name -Unique
+Get-Process | Select-Object -ExpandProperty Name
+Get-Process | Select-Object -First 4
+Get-Process | Select-Object -Property * -ExcludeProperty Id
+
+Get-Process | Group-Object -Property Name
+
+Get-Process -Name 'conhost'
+Get-Process | Where-Object -Property Name -EQ -Value 'conhost'
+Get-Process | Where-Object {$_.Name -eq 'conhost'}
+
+Get-Process | ForEach-Object {Write-Host "Process name is $($_.name)"}
+Get-ChildItem -Path 'C:\Temp\ToEncrypt' | ForEach-Object {$_.Encrypt()}
+Get-ChildItem -Path 'C:\Temp\ToEncrypt' | ForEach-Object -MemberName Encrypt
+
+#output formatting
+Get-Process | Format-Table -Property Name,Id
+Get-Service | Format-Table -Property Name,DisplayName,Status -AutoSize -Wrap
+
+Get-Process | Format-List -Property *
+
+Get-Process | Format-Wide -Property id
+
+#output processing
+Get-Process | Out-File 'C:\Temp\processes1.txt'
+Get-Process | Out-File 'C:\Temp\processes1.txt' -Append
+Get-Process | Out-Host -Paging
+Get-Process | Out-Printer -Name 'MyPrinter1'
+Get-Process | Out-GridView
+Get-Process | Out-GridView -Title 'Processes'  -PassThru | Out-GridView
+
+Get-Process | ConvertTo-Csv | Out-File 'C:\temp\processes2.csv'
+Get-Process | Export-CSV 'C:\temp\processes2.csv' -Delimiter "`t"
+Get-Process | Select-Object -f 1 -Property name,id | ConvertTo-Json
+
+Get-Process | Select-Object -f 3 -Property name,id | ConvertTo-Html -Title 'Processes' -PreContent (Get-Date) | Out-File 'C:\temp\processes.htm'
