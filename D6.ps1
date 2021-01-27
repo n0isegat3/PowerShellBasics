95fd3bbcc4affcc5043e2256d126a48d1d957285
n0isegat3 <jan@cyber-rangers.com>
2021-01-27 10:56:35 +0100

    u

--- D6.ps1
+++ D6.ps1
@@ -71,3 +71,46 @@ Invoke-Command -ComputerName server1 -ScriptBlock {
 Invoke-Command -ComputerName server1 -ScriptBlock {
     Get-Job | Receive-Job
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+$ScheduledJobOption = New-ScheduledJobOption -RunElevated -HideInTaskScheduler -StartIfOnBattery
+
+$trigger = New-JobTrigger -AtLogOn
+
+
+Register-ScheduledJob -Name CreateRandomFolder -Trigger $trigger -ScheduledJobOption $ScheduledJobOption -ScriptBlock {
+    New-Item -ItemType Directory -Path C:\ -Name (Get-Random)
+}
+
+Get-ScheduledJob
+Get-ScheduledJob | Get-ScheduledJobOption
+Get-ScheduledJob | Select-Object -ExpandProperty Command
+
+Get-Job | Receive-Job
+
+Get-Job | select Id,Name,PSEndTime,PSBeginTime
+Get-Job | Sort-Object -Property PSEndTime | select Id,Name,PSEndTime -Last 1
+Get-Job | Sort-Object -Property PSEndTime | select -Last 1 |Receive-Job
+
+#filepath of job config: $env:userprofile\AppData\Local\Microsoft\Windows\PowerShell\ScheduledJobs\CreateRandomFolder
