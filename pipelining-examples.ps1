74305b67b4dc6f529c568d5d8f24cd318da64bfc
n0isegat3 <jan@cyber-rangers.com>
2021-03-11 10:01:19 +0100

    u

---
+++ pipelining-examples.ps1
@@ -0,0 +1,23 @@
+﻿#naplneni property v zavislosti na hodnote jine property
+Get-Process | Select-Object ProcessName,`
+    @{n='id procesu chrome';e={if ($_.processname -eq 'chrome') {$_.id} else {-1}}}
+
+
+
+# union dvou kolekci objektu
+notepad C:\temp\processinfo.csv #pouze pro vyrobeni dat. to csv bude mit nasledujici obsah:
+<#
+processnamevinfu	info
+chrome	prohlizec
+svchost	tech je moc
+#>
+$info = Import-Csv C:\temp\processinfo.csv -Delimiter "`t"
+
+Get-Process | select ProcessName,Id,@{n='info';e={
+
+    $procname = $_.processname
+
+    $info | Where-Object {$_.processnamevinfu -eq $procname} | Select-Object -ExpandProperty info
+
+    }
+}
